99bbf700f8adcbb3152d48200e8904c9aa1c4dd7
Aji Mustofa <asuarif5@gmail.com>
2020-04-16 17:08:56 +0700

    add comment for more information

--- shop.js
+++ shop.js
@@ -1,25 +1,27 @@
-const burger = document.querySelector('.burger');
+const burger = document.querySelector( '.burger' );
 
-burger.addEventListener('click', () => {
-  document.querySelector('.navbar-menu').classList.toggle('is-active');
-});
+burger.addEventListener( 'click', () => {
+  document.querySelector( '.navbar-menu' ).classList.toggle( 'is-active' );
+} );
 
+
+// Class  Shop
 class Shop {
-  constructor(title, harga, gambar) {
+  constructor( title, harga, gambar ) {
     this.title = title;
     this.harga = harga;
     this.gambar = gambar;
   }
 
   addToCart() {
-    let row = document.createElement('div');
-    let items = document.querySelector('.items');
-    let titleItem = document.querySelectorAll('.pepega');
-    titleItem.forEach(t => {
-      if (t.textContent === this.title) {
-        return alert('Item ini Sudah Ada');
+    let row = document.createElement( 'div' );
+    let items = document.querySelector( '.items' );
+    let titleItem = document.querySelectorAll( '.pepega' );
+    titleItem.forEach( t => {
+      if ( t.textContent === this.title ) {
+        return alert( 'Item ini Sudah Ada' );
       }
-    });
+    } );
     let rowKonten = `
     <div class="item-baris">
       <div class="title-img">
@@ -34,67 +36,67 @@ class Shop {
     </div>
     `;
     row.innerHTML = rowKonten;
-    items.appendChild(row);
-    row.querySelector('.hapus').addEventListener('click', Shop.removeItem);
-    row.querySelector('.quantiti').addEventListener('click', Shop.quantiti);
+    items.appendChild( row );
+    row.querySelector( '.hapus' ).addEventListener( 'click', Shop.removeItem );
+    row.querySelector( '.quantiti' ).addEventListener( 'click', Shop.quantiti );
   }
 
   static updateHarga() {
-    let items = document.querySelector('.items');
-    let itemBaris = document.querySelectorAll('.item-baris');
+    let items = document.querySelector( '.items' );
+    let itemBaris = document.querySelectorAll( '.item-baris' );
     let total = 0;
-    for (let i = 0; i < itemBaris.length; i++) {
-      let item = itemBaris[i];
-      let quantity = item.querySelectorAll('.quantiti')[0].value;
-      let harga = item.querySelectorAll('.harga')[0];
-      let realHarga = parseFloat(harga.textContent.replace('Rp', ''));
+    for ( let i = 0; i < itemBaris.length; i++ ) {
+      let item = itemBaris[ i ];
+      let quantity = item.querySelectorAll( '.quantiti' )[ 0 ].value;
+      let harga = item.querySelectorAll( '.harga' )[ 0 ];
+      let realHarga = parseFloat( harga.textContent.replace( 'Rp', '' ) );
       total += realHarga * quantity;
     }
-    total = Math.floor(total * 1000) / 1000;
-    document.querySelector('.totalgan').innerHTML = `Rp${total}`;
+    total = Math.floor( total * 1000 ) / 1000;
+    document.querySelector( '.totalgan' ).innerHTML = `Rp${total}`;
   }
 
-  static removeItem(e) {
+  static removeItem( e ) {
     e.target.parentElement.parentElement.remove();
     Shop.updateHarga();
   }
 
-  static quantiti(e) {
+  static quantiti( e ) {
     const quantity = e.target;
-    if (quantity.value == 0) {
+    if ( quantity.value == 0 ) {
       quantity.value = 1;
     }
     Shop.updateHarga();
   }
 }
 
-(function shop() {
-  const pesan = document.querySelectorAll('.pesan');
-  pesan.forEach(btnPesan => {
-    btnPesan.addEventListener('click', keranjang);
-  });
+( function shop() {
+  const pesan = document.querySelectorAll( '.pesan' );
+  pesan.forEach( btnPesan => {
+    btnPesan.addEventListener( 'click', keranjang );
+  } );
 
-  const tombol_beli = document.querySelector('.tombol_beli');
-  tombol_beli.addEventListener('click', removeCart);
+  const tombol_beli = document.querySelector( '.tombol_beli' );
+  tombol_beli.addEventListener( 'click', removeCart );
   Shop.updateHarga();
-})();
+} )();
 
-function keranjang(e) {
+function keranjang( e ) {
   let tombol = e.target;
   let item = tombol.parentElement.parentElement;
-  let title = item.querySelectorAll('.nama')[0].textContent;
-  let harga = item.querySelectorAll('.harga-makan')[0].textContent;
-  let gambar = item.querySelectorAll('.ee')[0].src;
-  let produk = new Shop(title, harga, gambar);
+  let title = item.querySelectorAll( '.nama' )[ 0 ].textContent;
+  let harga = item.querySelectorAll( '.harga-makan' )[ 0 ].textContent;
+  let gambar = item.querySelectorAll( '.ee' )[ 0 ].src;
+  let produk = new Shop( title, harga, gambar );
   produk.addToCart();
   Shop.updateHarga();
 }
 
 function removeCart() {
-  alert('Terima kasih sudah membeli');
-  let items = document.querySelector('.items');
-  while (items.hasChildNodes()) {
-    items.removeChild(items.firstChild);
+  alert( 'Terima kasih sudah membeli' );
+  let items = document.querySelector( '.items' );
+  while ( items.hasChildNodes() ) {
+    items.removeChild( items.firstChild );
   }
   Shop.updateHarga();
 }
